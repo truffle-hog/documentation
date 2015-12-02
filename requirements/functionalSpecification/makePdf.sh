e5b8bfbe30dcb653ffef528a8124f280f3fb533f
Jan Hermes <jan.hermes@hotmail.de>
2015-12-02 19:54:35 +0100

    repaired the linux makePdf.sh skript, works now (building the glossar entry in the TOC)

--- requirements/functionalSpecification/makePdf.sh
+++ requirements/functionalSpecification/makePdf.sh
@@ -1,4 +1,7 @@
+rm *.bbl *.aux *.glo *.gls *.ist *.ilg *.log *.out *.blg *.synctex.gz *.toc
+
 pdflatex functionalSpecificationPS.tex
 makeindex -s functionalSpecificationPS.ist -o functionalSpecificationPS.gls functionalSpecificationPS.glo
+pdflatex functionalSpecificationPS.tex
 pdflatex functionalSpecificationPS.tex
 evince functionalSpecificationPS.pdf
