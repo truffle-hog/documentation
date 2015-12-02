9b8d9cf9b3429ef475954fda24b42f97bff4ae45
Infinity95 <mark.giraud@t-online.de>
2015-12-02 20:12:44 +0100

    GUI und Systemmodelle Glossareinträge hinzugefügt. Powershell Skript verbessert. Unnötige Datei entfernt.

--- requirements/functionalSpecification/makePdf.ps1
+++ requirements/functionalSpecification/makePdf.ps1
@@ -1,4 +1,11 @@
-pdflatex functionalSpecificationPS.tex
-makeindex -s functionalSpecificationPS.ist -o functionalSpecificationPS.gls functionalSpecificationPS.glo
-pdflatex functionalSpecificationPS.tex
-start functionalSpecificationPS.pdf
+$mainfile = "functionalSpecificationPS" # Name of the file to compile. This will also be the outpit file name.
+$removeAuxiliaries = $TRUE # Remove all temp/aux files?
+
+If($removeAuxiliaries) {
+	rm *.aux, *.gls, *.gls, *.ilg, *.ist, *.pdf, *.toc, *.glo # Clean temp files
+}
+Invoke-Expression "pdflatex $mainfile.tex"
+Invoke-Expression "makeindex -s $mainfile.ist -o $mainfile.gls $mainfile.glo" # Build glossary
+Invoke-Expression "pdflatex $mainfile.tex"
+Invoke-Expression "pdflatex $mainfile.tex"
+Invoke-Expression "start $mainfile.pdf" # Show the pdf
