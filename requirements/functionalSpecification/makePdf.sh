ebd4230600faefca093c334bd388dd3912f7ffc1
Jan Hermes <jan.hermes@hotmail.de>
2015-12-12 12:52:40 +0100

    changed makePdf.sh to work with the new style of lists

--- requirements/functionalSpecification/makePdf.sh
+++ requirements/functionalSpecification/makePdf.sh
@@ -1,9 +1,12 @@
 rm *.bbl *.aux *.glo *.gls *.ist *.ilg *.log *.out *.blg *.synctex.gz *.toc *.lof
 
 pdflatex functionalSpecificationPS.tex
+makeglossaries functionalSpecificationPS
+pdflatex functionalSpecificationPS.tex
+
 makeindex -s functionalSpecificationPS.ist -o functionalSpecificationPS.gls functionalSpecificationPS.glo
 pdflatex functionalSpecificationPS.tex
 pdflatex functionalSpecificationPS.tex
-makeindex -s functionalSpecificationPS.ist -o functionalSpecificationPS.gls functionalSpecificationPS.glo
-pdflatex functionalSpecificationPS.tex
+#makeindex -s functionalSpecificationPS.ist -o functionalSpecificationPS.gls functionalSpecificationPS.glo
+#pdflatex functionalSpecificationPS.tex
 evince functionalSpecificationPS.pdf
