85678c50b203763bcb85a5dc01138e935fe8036d
Infinity95 <mark.giraud@t-online.de>
2015-12-12 17:27:01 +0100

    Skript angepasst

--- requirements/functionalSpecification/makePdf.ps1
+++ requirements/functionalSpecification/makePdf.ps1
@@ -2,17 +2,17 @@ $mainfile = "functionalSpecificationPS" # Name of the file to compile. This will
 $removeAuxiliaries = $TRUE # Remove all temp/aux files?
 
 If($removeAuxiliaries) {
-	rm *.aux, *.gls, *.gls, *.ilg, *.ist, *.pdf, *.toc, *.glo, *.glg, *.out, *.lof # Clean temp files
+	rm *.aux, *.gls, *.gls, *.ilg, *.ist, *.pdf, *.toc, *.glo, *.glg, *.out, *.lof, *.acn, *.acr, *.alg # Clean temp files
 }
 If($?){
 	Invoke-Expression "pdflatex $mainfile.tex"
-	Invoke-Expression "makeindex -s $mainfile.ist -o $mainfile.gls $mainfile.glo" # Build glossary
+	Invoke-Expression "makeglossaries $mainfile"
 	Invoke-Expression "pdflatex $mainfile.tex"
 	Invoke-Expression "pdflatex $mainfile.tex"
-	Invoke-Expression "makeindex -s $mainfile.ist -o $mainfile.gls $mainfile.glo" # Build glossary
+	Invoke-Expression "makeglossaries $mainfile"
 	Invoke-Expression "pdflatex $mainfile.tex"
 	Invoke-Expression "start $mainfile.pdf" # Show the pdf
 }
 If($removeAuxiliaries) {
-	rm *.aux, *.gls, *.gls, *.ilg, *.ist, *.toc, *.glo, *.glg, *.out, *.lof # Clean temp files but not pdf
+	rm *.aux, *.gls, *.gls, *.ilg, *.ist, *.toc, *.glo, *.glg, *.out, *.lof, *.acn, *.acr, *.alg # Clean temp files but not pdf
 }
