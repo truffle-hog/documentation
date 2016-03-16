041e8bf8b7afeda6811166568e14c1241d9188e5
Maximilian Diez <maximilian.diez@student.kit.edu>
2016-03-16 17:03:58 +0100

    added third presentation with all old resources (why not and somebody might need those anyway)

---
+++ presentation3/functionalSpecification/makePdf.sh
@@ -0,0 +1,14 @@
+rm *.bbl *.aux *.glo *.gls *.ist *.ilg *.log *.out *.blg *.synctex.gz *.toc *.lof *.acn *.acr *.glg *.xdy *.alg
+
+pdflatex functionalSpecificationPS.tex
+makeglossaries functionalSpecificationPS
+pdflatex functionalSpecificationPS.tex
+pdflatex functionalSpecificationPS.tex
+makeglossaries functionalSpecificationPS
+pdflatex functionalSpecificationPS.tex
+#makeindex -s functionalSpecificationPS.ist -o functionalSpecificationPS.gls functionalSpecificationPS.glo
+#pdflatex functionalSpecificationPS.tex
+#pdflatex functionalSpecificationPS.tex
+#makeindex -s functionalSpecificationPS.ist -o functionalSpecificationPS.gls functionalSpecificationPS.glo
+#pdflatex functionalSpecificationPS.tex
+evince functionalSpecificationPS.pdf
